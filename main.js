100cbb107b13baeb94be7989638e3e21398d29c7
NickProdromou <hello@nickpro.com.au>
2016-06-18 22:57:51 +1000

    added a readme, cleaned up style further,made counter to show active items

--- main.js
+++ main.js
@@ -1,42 +1,41 @@
-/* DOM Manipulation: Independent Practice
-
-To complete this excercise, you must meet the following requirements:
-
-- When the user clicks the "#new-thing-button" button, add whatever is in the input box to the "#my-list" list.
-- Only add an item if the input box is not blank.
-- Clear the input box when the user clicks the button.
-
-Here are some bonus tasks to push your DOM knowledge!
-- Bonus tasks:
-  - When a list item is archived, change its background colour to "green"
-  - Add a link to each item to delete it completely
-  - Instead of deleting it completely, move it to a second list called "Archive"
-
-*/
 
 //Varibles
 var newButton = document.querySelector("#new-thing-button");
 var myList = document.querySelector("#my-list");
 var archived = document.querySelector("#Archive");
 var listItem = document.querySelectorAll(".list-thing");
+var activeCount = document.querySelector("#counter");
+
+
 
 //functions
 appendTo = function(box,targetEl) {
   targetEl.appendChild(box);
 }
 
+function getCurrentCount() {
+  var currentCount = document.querySelectorAll(".list-thing").length;
+  activeCount.innertext += "null ";
+  activeCount.innerText = " " + currentCount;
+}
+
 function watchItems() {
+
 for(var i = 0; i < myList.children.length; i++) {
 
+    //mouseover handler
     myList.children[i].addEventListener('mouseover', function(){
         this.classList.add("active");
+        //mouseout handler
+        this.addEventListener('mouseout', function(){
+          this.classList.remove("active");
+        })
+        //click hanlder
         this.addEventListener('click',function(){
           appendTo(this,archived);
           this.classList.add("archived");
-        })
-        this.addEventListener('mouseout', function(){
-          console.log("mouse-left")
-          this.classList.remove("active");
+          this.classList.remove("list-thing");
+          getCurrentCount();
         })
       });
     }
@@ -50,12 +49,14 @@ newButton.addEventListener('click', function(event){
     if (userInput.value !== "") {
       newItem.innerText = userInput.value;
       newItem.className = "list-thing";
-      myList.appendChild(newItem);
+      appendTo(newItem, myList);
       userInput.value = "";
       newItem.classList.remove("active");
       watchItems();
+      getCurrentCount();
     }
 })
 
 
 watchItems();
+getCurrentCount();
